7c2827f57d1cb8ae07fc1ce089c80ee1251c7e25
BrayamPineda <98662662+BrayamPineda@users.noreply.github.com>
2022-02-10 20:36:06 -0600

    Add files via upload

---
+++ Ejercicios de los videos/Ejercicio 3 - variables/Ejercicio3 Variables.cpp
@@ -0,0 +1,21 @@
+/*
+Alumno: Brayam Ovidio Pineda Hernandez
+cuenta: 20182002211
+*/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int numeroEntero;
+	numeroEntero = 15;
+	
+	cout<< "Entero: "<<numeroEntero <<endl;
+	
+	numeroEntero = 18;
+	
+	cout << "Entero modificado: "<<numeroEntero;
+	
+	return 0;
+}
