133d7e2755aec3f0e702155d749152f445894346
BrayamPineda <98662662+BrayamPineda@users.noreply.github.com>
2022-02-10 20:37:39 -0600

    Add files via upload

---
+++ Ejercicios de los videos/Ejercicio 4 - Imprimir valores de todas las variables/Todaslasvariables.cpp
@@ -0,0 +1,26 @@
+/*
+Alumno: Brayam Ovidio Pineda Hernandez
+cuenta: 20182002211
+*/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	bool valorBoolean;
+	valorBoolean = false;
+	
+	int valorEntero = 15;
+	double valorDouble = 20.99;
+	string  valorString = "Hola como estan?";
+	char valorChar = 'B';
+	
+	cout << "Valor Boolean : "<<valorBoolean <<endl;
+	cout << "Valor Entero : "<<valorEntero <<endl;
+	cout << "Valor Double : "<<valorDouble <<endl;
+	cout << "Valor String : "<<valorString <<endl;
+	cout << "Valor Char : "<<valorChar <<endl;
+	
+	return 0;
+}
