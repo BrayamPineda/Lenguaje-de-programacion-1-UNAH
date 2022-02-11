f68e1a57aec98fdeac32da4fe3dc616c057f588e
BrayamPineda <98662662+BrayamPineda@users.noreply.github.com>
2022-02-10 20:34:03 -0600

    Add files via upload

---
+++ Ejercicios de los videos/Ejercicio 2- Hola con nombre/main.cpp
@@ -0,0 +1,21 @@
+/*
+Alumno: Brayam Ovidio Pineda Hernandez
+cuenta: 20182002211
+*/
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+using namespace std;
+int main(int argc, char** argv) {
+	
+	string nombre;
+	
+	cout<< "Ingrese el nombre : ";
+	cin>> nombre;
+	
+	cout<< endl;
+	cout << "Su nombre es: " << nombre;
+	cout <<endl; 
+	return 0;
+}
