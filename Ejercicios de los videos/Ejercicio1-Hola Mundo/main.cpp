886371548bec9aa4c32770df92ff55bd0aa3d84a
BrayamPineda <98662662+BrayamPineda@users.noreply.github.com>
2022-02-10 20:31:11 -0600

    Add files via upload

---
+++ Ejercicios de los videos/Ejercicio1-Hola Mundo/main.cpp
@@ -0,0 +1,13 @@
+/*
+Alumno: Brayam Ovidio Pineda Hernandez
+cuenta: 20182002211
+*/
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+using namespace std;
+int main(int argc, char** argv) {
+	std::cout <<"Hola Mundo! ";
+	return 0;
+}
