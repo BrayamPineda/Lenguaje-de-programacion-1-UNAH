022326126214e26740c228bb00c1f26e91c0d536
BrayamPineda <98662662+BrayamPineda@users.noreply.github.com>
2022-02-11 19:13:28 -0600

    Add files via upload

---
+++ Ejercicios de los videos/Ejercicio 17 - Ciclo mientras infinito/Ciclo while infinito.cpp
@@ -0,0 +1,22 @@
+/*	
+	Autor: Brayam Ovidio Pineda Hernandez.
+	Número de cuenta : 20182002211
+*/
+
+ 
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int ciclos = 0;
+	
+	while (ciclos < 1000000)
+	{
+		cout << "*";
+		ciclos = ciclos + 1;
+	}
+	
+	return 0;
+}
