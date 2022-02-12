c7bf0a49e62ad383c93bb41fc626a493adc7ac61
BrayamPineda <98662662+BrayamPineda@users.noreply.github.com>
2022-02-11 20:28:13 -0600

    Add files via upload

---
+++ Ejercicios de los videos/Ejercicio 19 - Do Mientras/Do while.cpp
@@ -0,0 +1,30 @@
+/*	
+	Autor: Brayam Ovidio Pineda Hernandez.
+	Número de cuenta : 20182002211
+*/
+
+ 
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a = 2;
+	int b = 10;
+	
+	
+	cout << "...."
+	while (a > b)
+	{
+		cout << "(while) a > b"<<endl;
+		break;
+	}
+	
+	do {
+		cout << "(do while a > b)"<<endl;
+		break;
+	} while (a > b);
+	
+	return 0;
+}
