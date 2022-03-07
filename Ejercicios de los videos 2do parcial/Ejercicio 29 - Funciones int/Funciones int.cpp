2f9b30970c27102bae764e8f4c2eec94b95fd262
BrayamPineda <98662662+BrayamPineda@users.noreply.github.com>
2022-03-07 12:05:55 -0600

    Add files via upload

---
+++ Ejercicios de los videos 2do parcial/Ejercicio 29 - Funciones int/Funciones int.cpp
@@ -0,0 +1,20 @@
+/*
+Nombre: Brayam Ovidio Pineda Hernandez
+Numero de cuenta: 20182002211
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int sumar (int a,int b)
+{
+	return a + b;
+}
+
+int main ()
+{
+	system ("cls");
+	cout << sumar(5,7) <<endl;
+	return 0;
+}
