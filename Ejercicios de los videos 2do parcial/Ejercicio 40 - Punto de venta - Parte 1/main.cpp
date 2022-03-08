f903e0f092908dcf8aa00e5f4800e7dae39b1296
BrayamPineda <98662662+BrayamPineda@users.noreply.github.com>
2022-03-08 16:25:55 -0600

    Add files via upload

---
+++ Ejercicios de los videos 2do parcial/Ejercicio 40 - Punto de venta - Parte 1/main.cpp
@@ -0,0 +1,19 @@
+/*
+Nombre: Brayam Ovidio Pineda Hernandez
+Numero de cuenta: 20182002211
+*/
+
+#include <iostream>
+
+using namespace std;
+
+extern void menu();
+
+int main ()
+{
+	setlocale (LC_CTYPE, "Spanish");
+	
+	menu();
+	
+	return 0;
+}
