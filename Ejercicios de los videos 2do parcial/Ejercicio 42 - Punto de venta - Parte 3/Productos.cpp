f8150300de01da093b348542ca169b025ca15381
BrayamPineda <98662662+BrayamPineda@users.noreply.github.com>
2022-03-08 20:02:56 -0600

    Add files via upload

---
+++ Ejercicios de los videos 2do parcial/Ejercicio 42 - Punto de venta - Parte 3/Productos.cpp
@@ -0,0 +1,62 @@
+
+#include <iostream>
+
+using namespace std;
+
+extern void agregarProducto(string descripcion, int cantidad, double precio);
+
+void productos(int opcion)
+{
+	system ("cls");
+	
+	int opcionProducto = 0;
+	switch (opcion)
+	{
+		case 1:
+		{
+			cout<<"Bebidas calientes"<<endl;
+		    cout<<"*****************"<<endl;
+		    cout<<"1 - Capuccino "<<endl;
+		    cout<<"2 - Expresso" <<endl;
+		    cin>>opcionProducto;
+		    
+			switch (opcionProducto)
+			{
+				case 1:
+					agregarProducto("Capuccino",1,40);
+					break;
+				case 2:
+					agregarProducto("Expresso",1,30);
+					break;
+				default:
+					cout<<"Opcion no valida";
+					return;
+					break;
+			}
+				
+			break;
+		}
+		
+		case 2:
+		{
+			cout<<"Bebidas frias"<<endl;
+			cout<<"*************"<<endl;
+			system ("pause");
+			break;
+		}
+		
+		case 3:
+		{
+			cout<<"Reposteria"<<endl;
+			cout<<"**********"<<endl;
+			system ("pause");
+			break;
+		}
+		
+		{
+			default:
+			break;
+		}
+		
+	}
+}
---
+++ Ejercicios de los videos 2do parcial/Ejercicio 42 - Punto de venta - Parte 3/main.cpp
@@ -0,0 +1,17 @@
+/*
+Nombre: Brayam Ovidio Pineda Hernandez
+Numero de cuenta: 20182002211
+*/
+
+#include <iostream>
+
+using namespace std;
+
+extern void menu();
+
+int main (int argc, char const *argv[])
+{	
+	menu();
+	
+	return 0;
+}
