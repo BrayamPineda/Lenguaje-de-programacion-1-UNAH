6ee1e270d502e9b97555a811cead7ec04f5c7a14
BrayamPineda <98662662+BrayamPineda@users.noreply.github.com>
2022-03-16 20:01:09 -0600

    Add files via upload

---
+++ Tarea 7/main.cpp
@@ -0,0 +1,16 @@
+/*
+Nombre: Brayam Ovidio Pineda Hernandez
+Numero de cuenta: 20182002211
+*/
+
+#include <iostream>
+#include "Menu.h"
+
+using namespace std;
+
+int main (int argc, char const *argv[])
+{	
+	menu();
+	
+	return 0;
+}
