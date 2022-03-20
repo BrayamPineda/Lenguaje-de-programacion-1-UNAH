66fea7886d5f08a40dbebdf2172e872174a8a488
BrayamPineda <98662662+BrayamPineda@users.noreply.github.com>
2022-03-19 19:48:37 -0600

    Add files via upload

---
+++ Tarea 8/main.cpp
@@ -0,0 +1,15 @@
+/*
+Nombre: Brayam Ovidio Pineda Hernandez
+Numero de cuenta: 20182002211
+*/
+
+#include <iostream>
+#include "RaizCuadrada.h"
+
+using namespace std;
+
+int main (int argc, char const *argv[])
+{	
+	RaizCuadrada();
+	return 0;
+}
