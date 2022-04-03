77f73baec435cfe26bfa2bfe40298a5d8de2eda5
BrayamPineda <98662662+BrayamPineda@users.noreply.github.com>
2022-04-03 11:42:57 -0600

    Add files via upload

---
+++ Examen 2/Juego.h
@@ -0,0 +1,20 @@
+/*
+Nombre: Brayam Ovidio Pineda Hernandez.
+Numero de cuenta: 20182002211
+
+Examen 2
+
+*/
+void inicializarArreglo();
+void dificultad();
+void gotoxy(int x,int y);
+void pintar();
+void dibujarcuerpo();    
+void guardarposicion();
+void borrarcuerpo();
+void Teclear(char &tecla);
+void comida();
+bool gameover();
+void puntosx();
+void proceso(char &tecla, int &puntos);
+
